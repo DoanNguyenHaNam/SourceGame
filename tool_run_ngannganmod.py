9849eb4cf1a4ef21fe19676bdd58fb8400e85341
DOAN NGUYEN HA NAM <24521100@gm.uit.edu.vn>
2026-04-23 09:12:07 +0700

    Update tool_run_ngannganmod.py

--- tool_run_ngannganmod.py
+++ tool_run_ngannganmod.py
@@ -389,14 +389,6 @@ if True:
             output_blob[0:0] = b"\x22\x4a\x00\xef"
             output_blob+=b''
         return output_blob
-    def decompress_(strin,ZSTD_DICT=ZSTD_DICT):
-        posdecompress = strin.find(b"\x28\xb5\x2f\xfd")
-        if posdecompress != -1:
-            # strin = strin.replace(b'TELE: CHATPMIN', b'')
-            strin = strin[posdecompress:]
-            strin = strin[strin.find(b"\x28\xb5\x2f\xfd"):]
-            strin = pyzstd.decompress(strin, pyzstd.ZstdDict(ZSTD_DICT, True))
-        return strin
     def code_assetref(code,skinid):
         code=code.lower().replace(b'/'+skinid+b'/',b'/')
         skinid=skinid.decode()[:3]
